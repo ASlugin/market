843c4ea86fdb2c11a806a29937fd03e315a2d114
ASlugin <slugin.alexander@gmail.com>
2023-10-26 19:34:55 +0300

    Add openAPI documentation

--- src/main/java/ru/aslugin/punkmarket/configuration/ApplicationConfig.java
+++ src/main/java/ru/aslugin/punkmarket/configuration/ApplicationConfig.java
@@ -1,9 +1,20 @@
 package ru.aslugin.punkmarket.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@OpenAPIDefinition(
+    info = @Info(
+        title = "PUNK market backend API",
+        version = "1.0.0",
+        description = "API methods for PUNK market — service for the sale of products",
+        contact = @Contact(name = "Alexander Slugin", url = "https://github.com/ASlugin", email = "slugin.alexander@gmail.com")
+    )
+)
 public record ApplicationConfig(String test) {
 }
--- src/main/java/ru/aslugin/punkmarket/controller/Controller.java
+++ src/main/java/ru/aslugin/punkmarket/controller/Controller.java
@@ -1,8 +1,11 @@
 package ru.aslugin.punkmarket.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-
+    @GetMapping(value = "/")
+    public String getAllProducts() {
+        return "Hello, you called getAllProducts!";
+    }
 }
